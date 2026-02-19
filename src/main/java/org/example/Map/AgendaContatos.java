0233ff5292291d065c9df2fa058af3259540e707
Luiz Fernando <nandomagalhaes074@gmail.com>
2026-02-19 02:30:27 -0300

    terminado o estudo de Collections

---
+++ src/main/java/org/example/Map/AgendaContatos.java
@@ -0,0 +1,37 @@
+package org.example.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+        this.agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome){
+        if (!agendaContatoMap.isEmpty()){
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos(){
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome(String nome){
+        Integer numeroPorNome = null;
+        if (!agendaContatoMap.isEmpty()){
+           numeroPorNome = agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+
+
+}
---
+++ src/main/java/org/example/Map/Produto.java
@@ -0,0 +1,35 @@
+package org.example.Map;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, int quantidade, double preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
